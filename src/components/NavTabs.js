f2276d4eaa7e3b46faede298acdf4db62be25673
ngardunia <nickrhyse@gmail.com>
2023-03-06 15:23:42 -0500

    added code for NavTabs

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,1 +1,47 @@
 import React from 'react';
+
+function NavTabs({ currentPage, handlePageChange }) {
+    return (
+      <ul className="nav nav-tabs --bs-tertiary-bg-rgb">
+        <li className="nav-item">
+          <a
+            href="#home"
+            onClick={() => handlePageChange('Home')}
+            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+        >
+          Home
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#works"
+          onClick={() => handlePageChange('Works')}
+          className={currentPage === 'Works' ? 'nav-link active' : 'nav-link'}
+        >
+          Works
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
