09b108f05a4bb55be47a250a48a97ec97cd7034b
ngardunia <nickrhyse@gmail.com>
2023-03-06 15:32:47 -0500

    added code for PortfolioContainer

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -7,5 +7,26 @@ import Contact from './pages/Contact';
 
 export default function PortfolioContainer() {
     const [currentPage, setCurrentPage] = useState('Home');
+
+    const renderPage = () => {
+        if (currentPage === 'Home') {
+          return <Home />;
+        }
+        if (currentPage === 'About') {
+          return <About />;
+        }
+        if (currentPage === 'Works') {
+          return <Works />;
+        }
+        return <Contact />;
+      };
     
-};
+      const handlePageChange = (page) => setCurrentPage(page);
+    
+      return (
+        <div>
+          <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+          {renderPage()}
+        </div>
+      );
+    }
